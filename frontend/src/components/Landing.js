1a4a01450fb61bc35da3b4d4dabfbb73acafae49
sameersambhare <sameersambhare@gmail.com>
2024-07-02 22:03:57 +0530

    Second Commit

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,5 +1,10 @@
-import React from "react";
-import BannerImg from "../images/orange_2.png";
+import React, { useState } from "react";
+import OrangeAdd from "../images/orange_2.png";
+import BlackAdd from "../images/black-add.png";
+import PinkAdd from "../images/pink-add.png";
+import WhiteAdd from "../images/white-add.png";
+import BlueAdd from "../images/blue-add.png";
+import GreenAdd from "../images/green-add.png";
 import Black from "../images/black.png";
 import Orange from "../images/orange.png";
 import Pink from "../images/Pink.png";
@@ -7,6 +12,15 @@ import Green from "../images/green.png";
 import Blue from "../images/blue.png";
 import White from "../images/white.png";
 const Landing = () => {
+  const imageMenu = [
+    { photo: Orange, url: OrangeAdd },
+    { photo: Pink, url: PinkAdd },
+    { photo: Green, url: GreenAdd },
+    { photo: Blue, url: BlueAdd },
+    { photo: Black, url: BlackAdd },
+    { photo: White, url: WhiteAdd },
+  ];
+  const [photo,setPhoto]=useState(OrangeAdd)
   return (
     <div className="w-full min-h-screen bg-zinc-800 text-white mt-[116px]">
       {/* HEADING---------------------- */}
@@ -24,21 +38,20 @@ const Landing = () => {
       {/* BANNER----------------------------- */}
       <div className="banner-image px-8  overflow-hidden py-10">
         <img
-          src={BannerImg}
+          src={photo}
           alt="Banner"
           className="object-cover rounded-lg border-[0.1vw] border-[transparent]"
         />
         <div className="options-size w-full flex px-8 mt-10 justify-between items-center">
           <div className="options flex">
-            <img src={Orange} alt="" className="w-[85px]" />
-            <img src={Pink} alt="" className="w-[85px]" />
-            <img src={Green} alt="" className="w-[85px]" />
-            <img src={Blue} alt="" className="w-[85px]" />
-            <img src={Black} alt="" className="w-[85px]" />
-            <img src={White} alt="" className="w-[85px]" />
+            {imageMenu.map((item, index) => (
+              <img key={index} src={item.photo} alt="" onClick={()=>{setPhoto(item.url)}} className="w-[85px] cursor-pointer" />
+            ))}
           </div>
           <div className="selection-btn">
-            <p className=" capitalize py-[0.6vw] px-[4.5vw] text-[0.8vw] bg-white text-black rounded-full ">select your size</p>
+            <p className=" capitalize py-[0.6vw] px-[4.5vw] text-[0.8vw] bg-white text-black rounded-full ">
+              select your size
+            </p>
           </div>
         </div>
       </div>
